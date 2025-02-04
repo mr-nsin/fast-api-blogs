28ad97a64114d936ce6657c7112182f83a4d628a
mr-nsin <singhal.nitin94@gmail.com>
2025-02-04 17:12:02 +0530

    Add two endpoints where path parameters is different

--- main.py
+++ main.py
@@ -11,7 +11,16 @@ def index():
 @app.get('/about')
 def about():
     return {'data' : {'about': 'My name is nitin'}}
-    
+
+@app.get('/blog/{id}')
+def show(id: int):
+    print(type(id))
+    return {'data': id}
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': "unpublished"}
+
 if __name__=="__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=9000, reload=True)
