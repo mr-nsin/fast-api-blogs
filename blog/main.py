52617773157f76a4853165412c9e55b51dff8cb8
mr-nsin <singhal.nitin94@gmail.com>
2025-02-06 14:12:53 +0530

    Create database connection and create blogs table and it's related model in api

--- blog/main.py
+++ blog/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from schemas import Blog
+import models
+from database import engine
 
 app = FastAPI()
 
+models.Base.metadata.create_all(engine)
 
 @app.post('/blog')
 def create(request: Blog):
