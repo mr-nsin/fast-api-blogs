ae1d683ca7f1fc1fcf825d3a3066b96a495ca8f1
mr-nsin <singhal.nitin94@gmail.com>
2025-02-06 16:19:48 +0530

    New Blog Post method

--- blog/main.py
+++ blog/main.py
@@ -1,15 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from schemas import Blog
 import models
-from database import engine
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
 
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 @app.post('/blog')
-def create(request: Blog):
-    return request
+def create(request: Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title = request.title, body = request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
 
 
 if __name__ == '__main__':
