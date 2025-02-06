b878e7860c0a713564d78eb956d05180230c4517
= <=>
2025-02-07 00:32:05 +0530

    completed update api endpoint

--- blog/main.py
+++ blog/main.py
@@ -38,18 +38,20 @@ def show(id, response: Response, db: Session = Depends(get_db)):
        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with {id} is not found in database")
     return blog
 
+
 @app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def delete(id, db: Session = Depends(get_db)):
     db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
     db.commit()
     return 'done'
 
+
 @app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
 def update(id, request: BlogUpdate, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with {id} not found')
-    blog.update()
+    blog.update(request.dict())
     db.commit()
     return 'updated'
 
