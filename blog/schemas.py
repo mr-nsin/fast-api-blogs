f209dd351f22c632e7919d1f5c129f0997bca446
mr-nsin <singhal.nitin94@gmail.com>
2025-02-07 18:56:34 +0530

    Add user relationship in showblog to show user data

--- blog/schemas.py
+++ blog/schemas.py
@@ -10,11 +10,6 @@ class BlogUpdate(BaseModel):
     title: str
     body: str
 
-class ShowBlog(BaseModel):
-    title: str
-    body: str
-    class Config():
-        orm_mode = True
 
 class User(BaseModel):
     name: str
@@ -25,5 +20,12 @@ class ShowUser(BaseModel):
     name: str
     email: str
 
+    class Config():
+        orm_mode = True
+
+class ShowBlog(BaseModel):
+    title: str
+    body: str
+    creator : ShowUser
     class Config():
         orm_mode = True
