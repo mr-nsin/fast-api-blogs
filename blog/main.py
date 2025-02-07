cab033ba8958ec4226b61bcac1a53a338429ee83
mr-nsin <singhal.nitin94@gmail.com>
2025-02-07 17:03:04 +0530

    Create endpoint to get details of user

--- blog/main.py
+++ blog/main.py
@@ -67,6 +67,12 @@ def create_user(request: User, db: Session = Depends(get_db)):
     return new_user
 
 
+@app.get('/user/{id}', response_model=ShowUser)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id {id} is not available") 
+    return user
 
 if __name__ == '__main__':
     import uvicorn
