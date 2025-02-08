134b372dfd6b60ee586442134da6378ce3081734
= <=>
2025-02-08 23:28:52 +0530

    Use repository layer to breakdown fetch code from db in separate files

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -4,44 +4,31 @@ from schemas import ShowBlog, Blog, BlogUpdate
 from sqlalchemy.orm import Session
 from database import get_db
 import models
+from repository import blog
 
 router = APIRouter(prefix='/blog', tags=['Blogs'])
 
 @router.get('/', response_model = List[ShowBlog])
 def all(db: Session = Depends(get_db)):
-    blogs = db.query(models.Blog).all()
-    return blogs
+    return blog.get_all(db)
 
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
 def create(request: Blog, db: Session = Depends(get_db)):
-    new_blog = models.Blog(title = request.title, body = request.body, user_id = 1)
-    db.add(new_blog)
-    db.commit()
-    db.refresh(new_blog)
-    return new_blog
+    return blog.create_one(request, db)
 
 
 @router.get('/{id}', status_code=200, response_model=ShowBlog)
-def show(id, response: Response, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
-    if not blog:
-       raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with {id} is not found in database")
-    return blog
+def show(id: int, db: Session = Depends(get_db)):
+    return blog.show(id, db)
 
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def delete(id, db: Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
-    db.commit()
-    return 'done'
+def delete(id: int, db: Session = Depends(get_db)):
+    return blog.delete_one(id, db)
 
 
 @router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
-def update(id, request: BlogUpdate, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id)
-    if not blog.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with {id} not found')
-    blog.update(request.dict())
-    db.commit()
-    return 'updated'
+def update(id: int, request: BlogUpdate, db: Session = Depends(get_db)):
+    return blog.update_one(id, request, db)
+    
