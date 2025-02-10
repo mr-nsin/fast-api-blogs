f95969159079f21d10bd317aa9439217030a39f7
mr-nsin <singhal.nitin94@gmail.com>
2025-02-10 14:55:36 +0530

    Add login with returning access token using python jose library

--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from schemas import Login
 from database import get_db
 from sqlalchemy.orm import Session
+from hashing import Hash
+from bearertoken import create_access_token
 import models
 
 router = APIRouter(tags=['Authentication'])
@@ -11,4 +13,10 @@ def login(request: Login, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.email == request.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid credentials")
-    return 'login'
+    
+    if not Hash.verify(user.password, request.password):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Incorrect password')
+    
+    access_token = create_access_token(data={"sub": user.username})
+
+    return {"access_token": access_token, "token_type": "bearer"}
--- blog/schemas.py
+++ blog/schemas.py
@@ -38,3 +38,11 @@ class ShowBlog(BaseModel):
 class Login(BaseModel):
     username: str
     password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
