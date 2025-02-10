fda79cab28533a2eff9752c332a34b4e838bc6bb
mr-nsin <singhal.nitin94@gmail.com>
2025-02-10 15:04:17 +0530

    Add login with returning access token using python jose library

--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -17,6 +17,6 @@ def login(request: Login, db: Session = Depends(get_db)):
     if not Hash.verify(user.password, request.password):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Incorrect password')
     
-    access_token = create_access_token(data={"sub": user.username})
+    access_token = create_access_token(data={"sub": user.name})
 
     return {"access_token": access_token, "token_type": "bearer"}
